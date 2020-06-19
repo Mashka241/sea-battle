c860cccfb30c45c0572e990ae60e100bd22066a8
Masha Ivanova <ivanova.m241@gmail.com>
2020-06-19 09:48:34 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Header from './components/header/header'
+import Auth from './containers/auth/auth'
+import User from './containers/user/user'
+import Game from './containers/game/game'
+import Statistics from './containers/statistics/statistics'
+import HelloPage from './components/hello-page/hello-page'
 
-const App = () => {
+const App = ({ isAuth = true }) => {
+  let routes = (
+    <Switch>
+      <Route path='/auth' component={Auth} />
+      <Route path='/statistics' component={Statistics} />
+      <Route path='/' component={HelloPage} />
+    </Switch>
+  )
+  if (isAuth) {
+    routes = (
+      <Switch>
+        <Route path='/game' component={Game} />
+        <Route path='/statistics' component={Statistics} />
+        <Route path='/' component={User} />
+      </Switch>
+    )
+  }
   return (
     <div className='App'>
       <Header />
+      {routes}
     </div>
   )
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import classes from './header.module.scss'
 
 const Header = () => {
@@ -6,8 +7,12 @@ const Header = () => {
     <header className={classes.Header}>
       <nav className={classes.HeaderNav}>
         <ul className={classes.HeaderNavList}>
-          <li>game</li>
-          <li>statistics</li>
+          <li className={classes.HeaderNavListItem}>
+            <NavLink to='/'>main</NavLink>
+          </li>
+          <li className={classes.HeaderNavListItem}>
+            <NavLink to='/statistics'>statistics</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
